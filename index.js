4b7d28ca9a651e507d3af733110c4ba2ce8aa84b
notunderctrl <underctrl.io@gmail.com>
2023-06-14 01:11:45 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv/config');
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on('ready', () => console.log(`${client.user.username} is online.`));
+
+client.login(process.env.TOKEN);
